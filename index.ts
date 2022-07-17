9d4cdca6d772692208b29422437a5f265e4f72c9
Danielle Barker - Mac <caseybarker86@gmail.com>
2022-07-16 19:45:05 -0600

    testing something

--- index.ts
+++ index.ts
@@ -8,6 +8,20 @@ import { getLocalStream } from "./utilities/getLocalStream";
 // export all imports
 export {
   CallButtons,
+  /**
+   * Get local peer connection and get offer from firestore.
+   * Creates an answer and sets it in the room collection.
+   * Sets up listeners for the caller candidate collection
+   * @param {string} theRoomId
+   * @param {Firestore} db
+   * @param {MediaStream} localStream
+   * @param {MediaStream} remoteStream
+   * @param {string} roomCollectionName
+   * @param {string} calleeCandidatesCollectionName
+   * @param {string} callerCandidatesCollectionName
+   * @param {React.Dispatch<React.SetStateAction<undefined>>} setRemoteStream
+   * @returns {Promise<RTCPeerConnection>}
+   */
   getLocalPCandGetOffer,
   getLocalPCAndSetOffer,
   ToggleButtons,
